3638d909ba7e83fb59751c829f0aeb732d8fab57
ezku <eevert.saukkokoski@brainalliance.com>
2011-08-29 17:01:02 +0300

    fixed skin coloring not to fail horribly

--- wordpress/wp-content/themes/athene/athene.js
+++ wordpress/wp-content/themes/athene/athene.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
 		    'monochrome' : true
 		});
 	};
-	noisy('header').css('background-color', '#79B654');
-	noisy('#main').css('background-color', '#F3F4F0');
-	noisy('body, footer').css('background-color', '#1E2626');
+	noisy('header#branding');
+	noisy('#main');
+	noisy('body');
 });
