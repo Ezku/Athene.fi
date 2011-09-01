4c92f17f3d98405f2e1779d2caa453752aebff0e
Pyry Kröger <pyry@krogerit.net>
2011-09-01 22:02:33 +0300

    enabled back button in flickr browser

--- wordpress/wp-content/plugins/FlickrPhotosets/page.js
+++ wordpress/wp-content/plugins/FlickrPhotosets/page.js
@@ -1,3 +1,16 @@
 jQuery(function() {
   flickrbrowser.getPhotosets();
+  
+  jQuery(window).hashchange( function(){
+    var hashParams = flickrbrowser.explodeHash();
+    if (flickrbrowser.photosetClicked) { // do not trigger event when showing the photoset manually
+      flickrbrowser.photosetClicked = false;
+    } else if (hashParams.photoset) {
+      var inactive = jQuery('.photoset').not('#photoset'+hashParams.photoset);
+      inactive.removeClass('active');
+	    inactive.children(".photos").addClass('hide');
+	    flickrbrowser.getPhotoset(hashParams.photoset);
+	  }
+	  
+  })
 });
