ecdad44bbcf5031d2f5dc961e36439be8dcdfeba
Pyry Kröger <pyry@krogerit.net>
2011-09-20 14:16:14 +0300

    added container 404; removed archive page

--- wordpress/wp-content/themes/athene/404.php
+++ wordpress/wp-content/themes/athene/404.php
@@ -6,7 +6,7 @@
 
 get_header(); ?>
 
-	<div id="primary">
+	<div id="primary" class="container_16">
 		<div id="content" role="main">
 
 			<article id="post-0" class="post error404 not-found">
@@ -18,22 +18,6 @@ get_header(); ?>
 					<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching, or one of the links below, can help.', 'toolbox' ); ?></p>
 
 					<?php get_search_form(); ?>
-
-					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
-
-					<div class="widget">
-						<h2 class="widgettitle"><?php _e( 'Most Used Categories', 'toolbox' ); ?></h2>
-						<ul>
-						<?php wp_list_categories( array( 'orderby' => 'count', 'order' => 'DESC', 'show_count' => 'TRUE', 'title_li' => '', 'number' => '10' ) ); ?>
-						</ul>
-					</div>
-
-					<?php
-					$archive_content = '<p>' . sprintf( __( 'Try looking in the monthly archives. %1$s', 'toolbox' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-					?>
-
-					<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
 
 				</div><!-- .entry-content -->
 			</article><!-- #post-0 -->
