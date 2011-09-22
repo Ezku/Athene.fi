61545427d15d8ca1d5ee2142fa726e963c65ebd6
Pyry Kröger <pyry@krogerit.net>
2011-09-22 13:15:50 +0300

    added block-sized link to page widgets

--- wordpress/wp-content/plugins/athene-index-widget/templates/widget-pages.php
+++ wordpress/wp-content/plugins/athene-index-widget/templates/widget-pages.php
@@ -4,9 +4,11 @@
             <?php echo !empty($instance['title']) ? $instance['title'] : $page->post_title ?>
         </a></h2>
     </header>
-    <div class="widget-content">
-        <article class="page-intro">
-            <?php echo $page->intro ?>
-        </article>
-    </div>
+    <a href="<?php echo get_permalink($instance['page']) ?>" class="no-decoration" >
+        <div class="widget-content">
+            <article class="page-intro">
+                <?php echo $page->intro ?>
+            </article>
+        </div>
+    </a>
 </div>
